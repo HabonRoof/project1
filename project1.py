7bcd1ef35f89592d1a26d7fca8dec84866a42b14
HabonRoof <johnson35762@gmail.com>
2025-01-12 09:22:16 +0800

    Add alarm3

--- project1.py
+++ project1.py
@@ -22,6 +22,8 @@ def check_alarm(alarm_time):
 
 alarm_time = '09:01:00'
 alarm_time2 = '09:05:00'
+alarm_time3 = '09:10:00'
+
 
 while(True):
     
@@ -30,6 +32,9 @@ while(True):
 
     elif check_alarm(alarm_time2):
         print('\n ALARM2 is ringing')
+    
+    elif check_alarm(alarm_time3):
+        print('\n ALARM3 is ringing')
 
     else:
         print(f'Curent NTP time is {get_ntp_time()}')
